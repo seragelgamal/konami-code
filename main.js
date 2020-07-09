33934b0280ebe9099d43c0b2ce50bbc8991dd337
seragelgamal <55516965+seragelgamal@users.noreply.github.com>
2020-07-09 10:18:25 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
-// TITLE
+let keys = [];
 
+document.addEventListener('keydown', keyDownHandler);
+
+function keyDownHandler(e) {
+    keys.push(e.key);
+    if (keys.length > 4) {
+        keys.shift();
+    }
+    if (keys.join('').toLowerCase() == 'code') {
+        document.title = 'dghsdhvshagbhdasvdb';
+    }
+}
